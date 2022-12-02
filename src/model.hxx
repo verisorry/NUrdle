bed2df40a7c49c591fac0de2bb16bf22e847534d
Kellen Lai <69728468+klai4444@users.noreply.github.com>
2022-12-01 20:05:50 -0800

    moved controller v to main

--- src/model.hxx
+++ src/model.hxx
@@ -23,22 +23,11 @@ public:
     void update_guess(int i, char input) {
         user_guess_[i] = input;
     }
-<<<<<<< Updated upstream
 
-=======
->>>>>>> Stashed changes
     void update_num_letters() {
         num_letters_in_guess = num_letters_in_guess + 1;
     }
 
-<<<<<<< Updated upstream
-    void submit_guess() {
-        submit_guess_ = true;
-    }
-
-=======
-    // bool valid_guess(char* filename, char guess[5]);
->>>>>>> Stashed changes
     bool valid_guess();
 
     void hit_key(char actual);
@@ -49,7 +38,6 @@ private:
     void load_word();
     char squares_[5][6];
     int num_letters_in_guess = 0;
-    bool submit_guess_;
     std::vector<std::string> dictionary_;
     std::string correct_word_;
     size_t next_word_index_ = 0;
